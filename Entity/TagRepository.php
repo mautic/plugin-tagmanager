fd2380d87f8aab5563771992285db2fbc1d80461
Rahul Dhande <68939488+rahuld-dev@users.noreply.github.com>
2023-06-01 08:33:54 +0530

    Merge pull request #1990 from acquia/MAUT-8642
    MAUT-8642 : Fix 500 error on pending contacts of segment email.

--- Entity/TagRepository.php
+++ Entity/TagRepository.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace MauticPlugin\MauticTagManagerBundle\Entity;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\ORM\NoResultException;
+use Mautic\LeadBundle\Entity\Tag;
 use Mautic\LeadBundle\Entity\TagRepository as BaseTagRepository;
 
 class TagRepository extends BaseTagRepository
@@ -54,8 +57,9 @@ class TagRepository extends BaseTagRepository
         }
 
         $q->where(
-            $q->expr()->in('ltx.tag_id', $tagIds)
+            $q->expr()->in('ltx.tag_id', ':tagIds')
         )
+            ->setParameter('tagIds', $tagIds, Connection::PARAM_INT_ARRAY)
             ->groupBy('ltx.tag_id');
 
         $result = $q->executeQuery()->fetchAllAssociative();
