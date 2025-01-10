446991fd65a6675c3392d4baa499a16fc3a773d9
Rahul Shinde <shinde.r.a@gmail.com>
2025-01-10 16:27:03 +0530

    Refactored global search for Focus and Tag

--- EventListener/SearchSubscriber.php
+++ EventListener/SearchSubscriber.php
@@ -5,21 +5,17 @@ declare(strict_types=1);
 namespace MauticPlugin\MauticTagManagerBundle\EventListener;
 
 use Mautic\CoreBundle\CoreEvents;
+use Mautic\CoreBundle\DTO\GlobalSearchFilterDTO;
 use Mautic\CoreBundle\Event\GlobalSearchEvent;
-use Mautic\CoreBundle\Security\Permissions\CorePermissions;
-use Mautic\LeadBundle\EventListener\GlobalSearchTrait;
+use Mautic\CoreBundle\Service\GlobalSearch;
 use MauticPlugin\MauticTagManagerBundle\Model\TagModel;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Twig\Environment;
 
 class SearchSubscriber implements EventSubscriberInterface
 {
-    use GlobalSearchTrait;
-
     public function __construct(
         private TagModel $model,
-        private CorePermissions $security,
-        private Environment $twig,
+        private GlobalSearch $globalSearch,
     ) {
     }
 
@@ -32,30 +28,15 @@ class SearchSubscriber implements EventSubscriberInterface
 
     public function onGlobalSearch(GlobalSearchEvent $event): void
     {
-        if (!$this->security->isGranted('tagManager:tagManager:view')) {
-            return;
-        }
-
-        $searchString = $event->getSearchString();
-        if (empty($searchString)) {
-            return;
-        }
-
-        $items = $this->model->getEntities([
-            'filter'           => $searchString,
-            'start'            => 0,
-            'limit'            => GlobalSearchEvent::RESULTS_LIMIT,
-            'ignore_paginator' => true,
-            'with_total_count' => true,
-        ]);
-
-        $this->addGlobalSearchResults(
-            $this->twig,
-            $event,
-            $items,
-            'mautic.tagmanager.tag.header.index',
-            '@MauticTagManager/SubscribedEvents/Search/global.html.twig',
-            ['canEdit' => $this->security->isGranted('tagManager:tagManager:edit')]
+        $filterDTO = new GlobalSearchFilterDTO($event->getSearchString());
+        $results   = $this->globalSearch->performSearch(
+            $filterDTO,
+            $this->model,
+            '@MauticTagManager/SubscribedEvents/Search/global.html.twig'
         );
+
+        if (!empty($results)) {
+            $event->addResults('mautic.tagmanager.tag.header.index', $results);
+        }
     }
 }
